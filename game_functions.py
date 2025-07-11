e3981764204c28efd9216cdab9ca5c05c486f9fd
duking119 <huoyanfeng7@gmail.com>
2025-07-11 15:40:17 +0800

    all new

--- game_functions.py
+++ game_functions.py
@@ -1,4 +1,5 @@
 import sys
+from time import sleep
 
 import pygame
 from bullet import Bullet
@@ -118,11 +119,27 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 
-def update_aliens(ai_settings,ship, aliens):
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+    """相应被外星人碰到的飞船"""
+    # 将ship_left减1
+    stats.ships_left -= 1
+
+    # 清空外星人列表和子弹列表
+    aliens.empty()
+    bullets.empty()
+
+    # 创建一群新的外星人，并将飞船放到屏幕底端中央
+    create_fleet(ai_settings, screen, ship, aliens)
+    ship.center_ship()
+
+    # 暂停游戏
+    sleep(1.5)
+
+def update_aliens(ai_settings,stats, screen, ship, aliens, bullets):
     """检查是否有外星人位于屏幕边缘，并将整群外星人的位置更新"""
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
     
     # 检测外星人和飞船之间的碰撞
     if pygame.sprite.spritecollideany(ship, aliens):
-        print("Ship hit!!!")
+        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
